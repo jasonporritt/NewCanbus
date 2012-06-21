2ad097262b64612085d3446600840e1662afbc44
Jason Porritt <jason.porritt@atomicobject.com>
2012-06-20 22:29:31 -0400

    Starting a different version of the Canbus library better to my liking.

---
+++ PCanbus.cpp
@@ -0,0 +1,99 @@
+
+#include <Arduino.h>
+#include <stdio.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <pins_arduino.h>
+#include <inttypes.h>
+#include <global.h>
+#include <mcp2515.h>
+#include <defaults.h>
+#include <PCanbus.h>
+
+// Default filter -- all open
+const prog_uint8_t all_open_filters[] PROGMEM = 
+{
+	// Group 0
+	MCP2515_FILTER(0),				// Filter 0
+	MCP2515_FILTER(0),				// Filter 1
+	
+	// Group 1
+	MCP2515_FILTER_EXTENDED(0),		// Filter 2
+	MCP2515_FILTER_EXTENDED(0),		// Filter 3
+	MCP2515_FILTER_EXTENDED(0),		// Filter 4
+	MCP2515_FILTER_EXTENDED(0),		// Filter 5
+	
+	MCP2515_FILTER(0),				// Mask 0 (for group 0)
+	MCP2515_FILTER_EXTENDED(0),		// Mask 1 (for group 1)
+};
+
+/* C++ wrapper */
+PCanbusClass::PCanbusClass() {
+}
+
+void PCanbusClass::set_filters(const prog_uint8_t *filter) {
+  mcp2515_static_filter(filter);
+}
+
+//
+// Get the full message, including id and data
+//
+char PCanbusClass::message_rx(tCAN *message) {
+	if (mcp2515_read_status(SPI_RX_STATUS)) {
+    uint8_t result = mcp2515_get_message(message);
+    if (result) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+char PCanbusClass::message_rx(unsigned char *buffer) {
+  tCAN message;
+
+  if (mcp2515_check_message()) {
+    if (mcp2515_get_message(&message)) {
+      buffer[0] = message.data[0];
+      buffer[1] = message.data[1];
+      buffer[2] = message.data[2];
+      buffer[3] = message.data[3];
+      buffer[4] = message.data[4];
+      buffer[5] = message.data[5];
+      buffer[6] = message.data[6];
+      buffer[7] = message.data[7];								
+      return 1;
+    }
+  }
+  return 0;
+
+}
+
+char PCanbusClass::message_tx(uint16_t id, uint8_t data[]) {
+	tCAN message;
+
+  // May be a few defaults here that should be parameterized?
+	message.id = id;
+	message.header.rtr = 0;
+	message.header.length = 8;
+	message.data[0] = data[0];
+	message.data[1] = data[1];
+	message.data[2] = data[2];
+	message.data[3] = data[3];
+	message.data[4] = data[4];
+	message.data[5] = data[5];
+	message.data[6] = data[6];
+	message.data[7] = data[7];
+	
+	if (mcp2515_send_message(&message)) {
+		return 1;
+	}
+
+	return 0;
+}
+
+char PCanbusClass::init(unsigned char speed) {
+  return mcp2515_init(speed);
+}
+
+PCanbusClass PCanbus;
---
+++ PCanbus.h
@@ -0,0 +1,50 @@
+/**
+ * CAN BUS
+ *
+ * Copyright (c) 2010 Sukkin Pang All rights reserved.
+ */
+#include <mcp2515.h>
+
+#ifndef canbus__h
+#define canbus__h
+
+#define CANSPEED_125 	  7		// CAN speed at 125 kbps
+#define CANSPEED_250  	3		// CAN speed at 250 kbps
+#define CANSPEED_500	  1		// CAN speed at 500 kbps
+
+// 
+// OBDII data definitions
+#define ENGINE_COOLANT_TEMP 0x05
+#define ENGINE_RPM          0x0C
+#define VEHICLE_SPEED       0x0D
+#define MAF_SENSOR          0x10
+#define O2_VOLTAGE          0x14
+#define THROTTLE			      0x11
+#define MAP_SENSOR          0x0B
+#define ENGINE_OIL_TEMP     0x5C            
+#define FUEL_INJECTION_TIMING 0x5D
+
+// OBDII request & response definitions
+#define PID_REQUEST         0x7DF
+#define PID_REPLY			      0x7E8
+
+class PCanbusClass
+{
+  public:
+
+	PCanbusClass();
+  char init(unsigned char);
+	char message_tx(uint16_t id, uint8_t data[]);
+	char message_rx(unsigned char *buffer);
+	char message_rx(tCAN *message);
+
+  void set_filters(const prog_uint8_t *filter);
+
+  private:
+	
+};
+
+extern PCanbusClass PCanbus;
+// extern tCAN message;
+
+#endif
